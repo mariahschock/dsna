c5884021edfea79c3a319d6dd238ea5a50c86dd3
Mariah <mariahschock@gmail.com>
2022-09-20 17:07:38 -0700

    queue functions/tests passing

--- day-02/Queue.js
+++ day-02/Queue.js
@@ -1,3 +1,32 @@
-class Queue {}
+class Queue {
+  #list;
+
+  constructor(list) {
+    this.#list = list || [];
+  }
+
+  enqueue(item) {
+    this.#list.push(item);
+  }
+
+  dequeue() {
+    return this.#list.shift();
+  }
+
+  hasNext() {
+    return this.newArr.length ? true : false;
+  }
+
+  get count() {
+    return this.#list.length;
+  }
+
+  get next() {
+    return this.#list[0];
+  }
+  get newArr() {
+    return this.#list;
+  }
+}
 
 module.exports = { Queue };
